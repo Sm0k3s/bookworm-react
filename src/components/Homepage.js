e20ecfd1a68a48b76d8d36f82dbd946cdcd7cdfe
Steve Obaga <obaga4@gmail.com>
2019-05-29 19:28:56 +0300

    Add the delete button

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import Header from './Header';
 import AddCategory from './AddCategory';
 import { getCategories, getBooks } from '../constants/actions';
-import { Card, Button } from 'react-bootstrap';
+import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
 
 class Homepage extends Component {
   componentDidMount() {
@@ -17,8 +17,16 @@ class Homepage extends Component {
     }
   }
 
+  handleDelete = (e) => {
+    e.preventDefault();
+
+    console.log(e.target.value);
+  }
+
   render () {
+    const { books } = this.props;
   return (
+  
   <div className="home">
   <Header />
   <div className="container">
@@ -27,21 +35,30 @@ class Homepage extends Component {
        <p>
            Your favourite tool to track books you've read.
        </p>
-       <div>
+       <div className="row " style={{ display: 'flex'}}>
        
-        {this.props.books.map(book => 
-          (<Card className="text-center">
-          <Card.Header>{book.title}</Card.Header>
-            <Card.Body>
-                <Card.Title>{book.title}</Card.Title>
-                <Card.Text>
-                {book.synopsis}
-                </Card.Text>
-                <Button variant="primary" href={book.url}> huh</Button>
-            </Card.Body>
-            <Card.Footer className="text-muted">{book.category}</Card.Footer> 
-            </Card>))}
-        
+        { books.map(book => (
+            <Card style={{ width: '33.3%', justifyContent: 'space-around'}}>
+                <Card.Img variant="top" src="https://spsy6.files.wordpress.com/2015/09/bookworm.jpg" />
+                <Card.Body>
+                    <Card.Title>{book.title}</Card.Title>
+                    <Card.Text>
+                    {book.synopsis}
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <ListGroupItem>Author: {book.author}</ListGroupItem>
+                    <ListGroupItem>Category: {book.category}</ListGroupItem>
+                </ListGroup>
+                <Card.Body>
+                    <button value={book.url} onClick={this.handleDelete}>Edit</button>
+                    <button value={book.url} onClick={this.handleDelete}>Delete</button>
+                    {/* <Card.Link href="#" value={book.url} onClick={this.handleDelete}>Edit</Card.Link>
+                    <Card.Link href="#" name="delete" value={book.url} onClick={this.handleDelete}>Delete</Card.Link> */}
+                </Card.Body>
+            </Card>
+            ))
+        }
         </div>
    </div>
    </div>
